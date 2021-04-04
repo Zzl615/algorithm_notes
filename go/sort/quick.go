1ae652031b6f34a7534fdc1f226b06e154fd8545
Noaghzil@615 <2966785870@qq.com>
2021-04-04 21:55:14 +0800

    Refactor: 快排修复

--- go/sort/quick.go
+++ go/sort/quick.go
@@ -1,7 +1,15 @@
 package sort
 
 func QuickSort(nums []int, start int, end int) []int {
+	// 递归结束条件
+	if start > end {
+		return nums
+	}
+	// 分治的中枢
 	pivot := nums[start]
+	// 保留该轮的起止值
+	low := start
+	high := end
 	for start < end {
 		for start < end && nums[end] >= pivot {
 			end--
@@ -14,8 +22,7 @@ func QuickSort(nums []int, start int, end int) []int {
 		nums[end] = nums[start]
 	}
 	nums[start] = pivot
-	QuickSort(nums, start+1, end)
-	QuickSort(nums, 0, start-1)
+	QuickSort(nums, low, start-1)
+	QuickSort(nums, start+1, high)
 	return nums
-
 }
