2dddeb003bd4a13e3608c8f4810aeb641922c38c
Noaghzil@615 <2966785870@qq.com>
2021-04-05 18:02:25 +0800

    Refactor: 冒泡排序

--- go/sort/bubble.go
+++ go/sort/bubble.go
@@ -2,14 +2,19 @@ package sort
 
 import "fmt"
 
-func BubbleSort(nums []int64) []int64 {	
+func BubbleSort(nums []int64) []int64 {
+	var isChange bool = false
 	for i := 0; i < len(nums)-1; i++ {
+		if i > 0 && !isChange {
+			break
+		}
 		for j := i; j < len(nums)-1; j++ {
 			fmt.Println(i, j)
 			if j != len(nums)-1 && nums[j] > nums[j+1] {
 				temp := nums[j]
 				nums[j] = nums[j+1]
 				nums[j+1] = temp
+				isChange = true
 			}
 		}
 	}
--- go/sort/bubble_test.go
+++ go/sort/bubble_test.go
@@ -7,3 +7,16 @@ func TestBubbleSort(t *testing.T) {
 	BubbleSort(nums)
 	t.Log(nums)
 }
+
+func TestPositiveBubbleSort(t *testing.T) {
+	var nums = []int64{1, 2, 7, 9, 11, 39, 54}
+	BubbleSort(nums)
+	t.Log(nums)
+}
+
+// TODO：优化逆序集合
+func TestReverseBubbleSort(t *testing.T) {
+	var nums = []int64{54, 39, 11, 9, 7, 2, 1}
+	BubbleSort(nums)
+	t.Log(nums)
+}
