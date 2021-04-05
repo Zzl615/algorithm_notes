92ea857b4308b8b51dd4e5438276f371aa70160a
Noaghzil@615 <2966785870@qq.com>
2021-04-05 17:49:15 +0800

    Feat: 冒泡排序

--- go/sort/bubble.go
+++ go/sort/bubble.go
@@ -1,1 +1,17 @@
 package sort
+
+import "fmt"
+
+func BubbleSort(nums []int64) []int64 {	
+	for i := 0; i < len(nums)-1; i++ {
+		for j := i; j < len(nums)-1; j++ {
+			fmt.Println(i, j)
+			if j != len(nums)-1 && nums[j] > nums[j+1] {
+				temp := nums[j]
+				nums[j] = nums[j+1]
+				nums[j+1] = temp
+			}
+		}
+	}
+	return nums
+}
--- go/sort/bubble_test.go
+++ go/sort/bubble_test.go
@@ -1,1 +1,9 @@
 package sort
+
+import "testing"
+
+func TestBubbleSort(t *testing.T) {
+	var nums = []int64{1, 39, 2, 9, 7, 54, 11}
+	BubbleSort(nums)
+	t.Log(nums)
+}
