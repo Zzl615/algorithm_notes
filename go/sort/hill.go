41e30508b84526db0776a4485701d8b21d152fe6
Noaghzil@615 <2966785870@qq.com>
2021-04-07 08:09:17 +0800

    Feat: 希尔排序-go

--- go/sort/hill.go
+++ go/sort/hill.go
@@ -1,6 +1,27 @@
 package sort
 
+import "fmt"
+
 func HillSort(nums []int) []int {
-	
+	var length int = len(nums)
+	var gap int = 1
+	for gap < length/3 {
+		gap = gap*3 + 1
+	}
+	for gap > 0 {
+		for i := gap; i < length; i++ {
+			temp := nums[i]
+			for j := i - gap; j >= 0; j = j - gap {
+				if nums[i] < nums[j] {
+					nums[j+gap] = nums[j]
+				} else {
+					nums[j+gap] = temp
+					break
+				}
+			}
+		}
+		fmt.Println(gap, nums)
+		gap = gap / 3
+	}
 	return nums
 }
